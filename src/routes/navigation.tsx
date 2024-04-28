c4c4ee328fca35a9e6b5fccc838ed424f8abd483
Talles Alves <talves2503@gmail.com>
2024-04-28 16:18:34 -0300

    add static signup screen and navigation

--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/Home";
 import Login from "../screens/Login";
+import Signup from "../screens/Signup";
 
 const Stack = createStackNavigator();
 
@@ -17,6 +18,7 @@ export default function Navigation() {
         }}
       >
         <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Signup" component={Signup}/>
         <Stack.Screen name="Home" component={Home}/>
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/Signup/index.tsx
+++ src/screens/Signup/index.tsx
@@ -1,18 +1,19 @@
+import Button from "../../components/Button";
+import FormField from "../../components/FormField";
+import OAuthSection from "../../components/OAuthSection";
+import { ConnectText } from "../../components/OAuthSection/styles";
+import theme from "../../global/styles/theme";
+import { DecorLine } from "../Home/styles";
+import { ActionWrapper, Form, LoginContainer, Wrapper } from "../Login/styles";
+import { NavigationProp } from "@react-navigation/native";
 import Logo from "../../../assets/login/big-logo.svg"
-import { ActionWrapper, Form, LoginContainer, Wrapper } from "./styles"
-import OAuthSection from "../../components/OAuthSection"
-import { DecorLine } from "../Home/styles"
-import theme from "../../global/styles/theme"
-import Button from "../../components/Button"
-import FormField from "../../components/FormField"
-import { ConnectText } from "../../components/OAuthSection/styles"
-import { NavigationProp } from "@react-navigation/native"
 
-interface LoginProps {
+interface SignInProps {
   navigation: NavigationProp<any>; // This can vary depending on your setup
 }
 
-export default function Login({ navigation } : LoginProps) {
+
+export default function SignUp({ navigation }: SignInProps) {
   return (
     <Wrapper>
       <Logo width={80} height={80} />
@@ -23,23 +24,25 @@ export default function Login({ navigation } : LoginProps) {
 
         <Form>
           <ConnectText>
-            Ou conecte-se com um email:
+            Ou cadastre-se com um email:
           </ConnectText>
 
+          <FormField label="Nome:" />
           <FormField label="E-mail:" />
           <FormField label="Senha:" />
+          <FormField label="Confirme a senha:" />
 
           <ActionWrapper>
             <Button
-              text="Cadastrar"
+              text="Logar"
               bgColor={theme.colors.background}
               borderColor={theme.colors.secondary}
               textColor={theme.colors.secondary}
-              screen="Signup"
+              screen="Login"
               navigation={navigation}
             />
             <Button
-              text="Entrar"
+              text="Cadastrar"
               bgColor={theme.colors.secondary}
               textColor={theme.colors.background}
               screen="Home"
